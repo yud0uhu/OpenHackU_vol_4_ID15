5a9af0dc3b873daad5f0d85e61d4a67984b11744
yud0uhu <b2190280@photon.chitose.ac.jp>
2021-03-06 21:46:03 +0900

    fix requirements.txt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+import os
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -9,77 +10,43 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
-import os
 
 app = Flask(__name__)
 
-# 環境変数取得
-# LINE Developersで設定されているアクセストークンとChannel Secretをを取得し、設定します。
+#環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+@app.route("/")
+def hello_world():
+    return "hello world!"
 
-## 1 ##
-# Webhookからのリクエストをチェックします。
 @app.route("/callback", methods=['POST'])
 def callback():
-    # リクエストヘッダーから署名検証のための値を取得します。
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
-    # リクエストボディを取得します。
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-
     # handle webhook body
-　　# 署名を検証し、問題なければhandleに定義されている関数を呼び出す。
-try:
-    handler.handle(body, signature)
-　　# 署名検証で失敗した場合、例外を出す。
-except InvalidSignatureError:
-    abort(400)
-　　# handleの処理を終えればOK
-return 'OK'
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-## 2 ##
-###############################################
-# LINEのメッセージの取得と返信内容の設定(オウム返し)
-###############################################
-
-# LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合に、
-# def以下の関数を実行します。
-# reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。
-# 第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
-
+    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-
-    # reply_tokenが特定の値だった場合に処理終了
-    if event.reply_token == "00000000000000000000000000000000":
-        return
-
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
-@handler.add(MessageEvent, message=StickerMessage)
-def handle_video(event):
-
-    # reply_tokenが特定の値だった場合に処理終了
-    if event.reply_token == "ffffffffffffffffffffffffffffffff":
-        return
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="Sticker"))
-
-
-# ポート番号の設定
 if __name__ == "__main__":
-    #    app.run()
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
