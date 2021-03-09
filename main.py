a62d9bb9fc053c717cf951caae4fb439e7203081
root <root@DESKTOP-GNI3ULF.localdomain>
2021-03-10 01:20:12 +0900

    お試し機能実装

--- main.py
+++ main.py
@@ -1,64 +1,158 @@
-from flask import Flask, request, abort
+# -*- coding: utf-8 -*-
+import sys
 import os
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+from flask import Flask, request, abort, send_file
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, LocationMessage, LocationSendMessage, TextSendMessage, StickerSendMessage, MessageImagemapAction, ImagemapArea, ImagemapSendMessage, BaseSize
 )
-
+from io import BytesIO, StringIO
+from PIL import Image
+import requests
+import urllib.parse
+import numpy
+import math
 app = Flask(__name__)
-
-# 環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
-
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
 
-@app.route("/")
-def hello_world():
-    return "hello world!"
-
-
-@app.route("/callback", methods=['POST'])
+@app.route("/", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
-# 特定のキーワードに対して返信する、それ以外はオウム返しをする仕様
-# オウム返しで渡しているevent.message.textの文字列に母音の類語を渡したい
+
+pins = [
+    [35.690810, 139.704500, 'A1'],
+    [35.691321, 139.703438, 'A5'],
+    [35.691074, 139.705056, 'B2'],
+    [35.691172, 139.704962, 'B3'],
+    [35.691209, 139.704300, 'B4'],
+    [35.692279, 139.702208, 'B10'],
+    [35.690521, 139.705810, 'C4'],
+    [35.690621, 139.706777, 'C5'],
+    [35.691267, 139.706879, 'C6'],
+    [35.691502, 139.707242, 'C7'],
+    [35.693777, 139.706166, 'E1'],
+    [35.693143, 139.706104, 'E2'],
+    [35.689273, 139.703907, 'E5'],
+    [35.688629, 139.703212, 'E6'],
+    [35.688497, 139.703397, 'E7'],
+    [35.689831, 139.703384, 'E9'],
+    [35.689421, 139.701877, 'E10'],
+]
 
 
 @handler.add(MessageEvent, message=TextMessage)
-　　　　def handle_text_message(event):
-　　　　　　　　　　　　text = event.message.text
-　　　　　　　　　　　　if text in ['やかましいわ', '知らんがな', 'ザイフ']:
-　　　　　　　　　　　　　　　　　　　　line_bot_api.reply_message(
-    event.reply_token,
-    TextSendMessage(text="zaifでのビットコインの価格は"))
-　　　　　　　　　　　　else:
+def handle_message(event):
+    if event.message.text.isdigit():
+        line_bot_api.reply_message(
+            event.reply_token,
+            [
+                LocationSendMessage(
+                    title=pins[int(event.message.text)][2],
+                    address='東京都新宿区',
+                    latitude=pins[int(event.message.text)][0],
+                    longitude=pins[int(event.message.text)][1]
+                )
+            ]
+        )
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            [
+                TextSendMessage(text='位置情報を送ると近くで終電まで空いている駅一覧を教えるよ(※絵文字1) '),
+                TextSendMessage(text='line://nv/location'),
+            ]
+        )
+
+
+@app.route("/imagemap/<path:url>/<size>")
+def imagemap(url, size):
+    map_image_url = urllib.parse.unquote(url)
+    response = requests.get(map_image_url)
+    img = Image.open(BytesIO(response.content))
+    img_resize = img.resize((int(size), int(size)))
+    byte_io = BytesIO()
+    img_resize.save(byte_io, 'PNG')
+    byte_io.seek(0)
+    return send_file(byte_io, mimetype='image/png')
+
+
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_location(event):
+    lat = event.message.latitude
+    lon = event.message.longitude
+    zoomlevel = 18
+    imagesize = 1040
+    map_image_url = 'https://maps.googleapis.com/maps/api/staticmap?center={},{}&zoom={}&size=520x520&scale=2&maptype=roadmap&key={}'.format(
+        lat, lon, zoomlevel, 'YOUR_GOOGLE_API_KEY')
+    map_image_url += '&markers=color:{}|label:{}|{},{}'.format(
+        'blue', '', lat, lon)
+    center_lat_pixel, center_lon_pixel = latlon_to_pixel(lat, lon)
+    marker_color = 'red'
+    label = 'E'
+    pin_width = 60 * 1.5
+    pin_height = 84 * 1.5
+    actions = []
+    for i, pin in enumerate(pins):
+        target_lat_pixel, target_lon_pixel = latlon_to_pixel(pin[0], pin[1])
+        delta_lat_pixel = (target_lat_pixel -
+                           center_lat_pixel) >> (21 - zoomlevel - 1)
+        delta_lon_pixel = (target_lon_pixel -
+                           center_lon_pixel) >> (21 - zoomlevel - 1)
+        marker_lat_pixel = imagesize / 2 + delta_lat_pixel
+        marker_lon_pixel = imagesize / 2 + delta_lon_pixel
+        x = marker_lat_pixel
+        y = marker_lon_pixel
+        if(pin_width / 2 < x < imagesize - pin_width / 2 and pin_height < y < imagesize - pin_width):
+            map_image_url += '&markers=color:{}|label:{}|{},{}'.format(
+                marker_color, label, pin[0], pin[1])
+            actions.append(MessageImagemapAction(
+                text=str(i),
+                area=ImagemapArea(
+                    x=x - pin_width / 2,
+                    y=y - pin_height / 2,
+                    width=pin_width,
+                    height=pin_height
+                )
+            ))
+            if len(actions) > 10:
+                break
+    message = ImagemapSendMessage(
+        base_url='https://{}/imagemap/{}'.format(
+            request.host, urllib.parse.quote_plus(map_image_url)),
+        alt_text='地図',
+        base_size=BaseSize(height=imagesize, width=imagesize),
+        actions=actions
+    )
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        [
+            TextSendMessage(text='終電まで空いている出口一覧です(※絵文字2)'),
+            message
+        ]
+    )
+
+
+offset = 268435456
+radius = offset / numpy.pi
+
+
+def latlon_to_pixel(lat, lon):
+    lat_pixel = round(offset + radius * lon * numpy.pi / 180)
+    lon_pixel = round(offset - radius * math.log((1 + math.sin(lat *
+                                                               numpy.pi / 180)) / (1 - math.sin(lat * numpy.pi / 180))) / 2)
+    return lat_pixel, lon_pixel
+
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
