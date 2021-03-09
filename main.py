28569a7138ae6615a70a6fccd1d692b0e91e5411
root <root@DESKTOP-GNI3ULF.localdomain>
2021-03-10 00:03:57 +0900

    突っ込みを返すように変更

--- main.py
+++ main.py
@@ -48,16 +48,13 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-　　　　　　　　def handle_text_message(event):
-　　　　　　　　　　　　　　　　　　　　　　　　text = event.message.text
-　　　　　　　　　　　　　　　　　　　　　　　　if text in ['やかましいわ', '知らんがな', 'ザイフ']:
-　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　line_bot_api.reply_message(
+　　def handle_text_message(event):
+　　　　　　text = event.message.text
+　　　　　　if text in ['やかましいわ', '知らんがな', 'ザイフ']:
+　　　　　　　　　　line_bot_api.reply_message(
     event.reply_token,
-    StickerSendMessage(
-        package_id='1',
-        sticker_id='1'
-    ))
-　　　　　　　　　　　　　　　　　　　　　　　　else:
+    TextSendMessage(text="zaifでのビットコインの価格は"))
+　　　　　　else:
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
