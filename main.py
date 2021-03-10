003c4fe2811e2d44806b8d8a80399ef88a87077f
root <root@DESKTOP-GNI3ULF.localdomain>
2021-03-10 09:19:23 +0900

    オウム返し状態に戻した

--- main.py
+++ main.py
@@ -1,31 +1,56 @@
+from flask import Flask, request, abort
 import os
-import json
 
-from flask import Flask, request
-
-from GenerateText import GenerateText
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 app = Flask(__name__)
 
+# 環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
-@app.route('/')
-def hello():
-    return 'Hello world! 2'
-
-
-@app.route('/rap', methods=['POST'])
-def rap():
-    generator = GenerateText()
-    if request.form:
-        verse = request.form['verse']
-    elif request.data:
-        jsons = str(request.data, encoding='utf-8')
-        data = json.loads(jsons)
-        verse = data['verse']
-
-    gen_txt = generator.generate(verse)
-    return gen_txt
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+@app.route("/")
+def hello_world():
+    return "hello world!"
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
