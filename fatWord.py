8e92478825ece1177da64500701dd4773c053c9c
root <root@DESKTOP-GNI3ULF.localdomain>
2021-03-12 01:47:25 +0900

    デフォルト応答メッセージと単語太らせクラスを実装

---
+++ fatWord.py
@@ -0,0 +1,152 @@
+import pykakasi
+import re
+import pandas as pd
+
+
+def message_generate(query):
+    # ユーザー側の入力ワード
+    target_word_origin = query
+    # ローマ字へ変換
+    kakasi = pykakasi.kakasi()
+
+    kakasi.setMode('H', 'a')
+    kakasi.setMode('K', 'a')
+    kakasi.setMode('J', 'a')
+
+    conv = kakasi.getConverter()
+    target_word = conv.do(target_word_origin)
+
+    # 母音のみ残す
+    target_word_vo = re.sub(r"[^aeiou]+", "", target_word)
+
+    # このdfの右辺に挿入するものを入れていく感じ
+    df = pd.read_csv('/home/denham/OpenHackU_vol_4_ID3/fatlist.csv')
+
+    df['ローマ字'] = df['原文'].apply(convert_to_romaji)
+    df['抽出'] = df['ローマ字'].apply(get_vowel)
+
+    # ここではpandas.core.series.Series型になっている
+    # print(type(df['抽出']))
+
+    # とりあえず配列にぶち込む
+    df['抽出'].values
+
+    # 要素がlist型として認識された！
+    # type(df['抽出'].values[0])
+    # print(df['抽出'].values[0])
+
+    i = 0
+    vowel_list = ' '
+    # 母音のリストを文字列に叩き込むためにfor文で順番に後ろから追加していく
+    for list in df['抽出'].values:
+        Str = "".join(df['抽出'].values[i])
+        vowel_list += Str+' '
+        i += 1
+
+    data_list = df['原文'].values
+    data_list_vo = df['ローマ字'].values
+
+    # vowel_dataのインデックスで母音変換前のdataが分かるように辞書作成。
+    dic = {k: v for k, v in enumerate(data_list)}
+
+    ranking = get_idx_score(data_list_vo, target_word_vo)
+    # print(data_list_vo)
+    # print(target_word_vo)
+    for i in range(len(ranking)):
+        idx = ranking[i][0]
+        score = ranking[i][1]
+        # print("スコア:" + str(score))
+        # print("言葉:" + dic[idx])
+        index = ranking[0][0]
+        max = dic[index]
+        result = max
+    return result
+
+# 短い方のwordをスライスし、他方にそれが含まれていた場合「韻」と見なし、その長さをスコアとして加算する
+
+
+def make_score(word_a, word_b):
+    score = 0
+    count = 0
+
+    # 読み込む文字列がリストよりも長い場合
+    if len(word_a) > len(word_b):
+
+      # 文字列が短すぎる場合、マイナス報酬を与える
+        score += (len(word_b) - len(word_a))*1.1
+
+        word_len = len(word_b)
+        for i in range(word_len):
+            for j in range(word_len + 1):
+                if word_b[i:j] in word_a:
+
+                    # 末尾の得点変化率を強くする
+                    if count == word_len-1:
+                        score += len(word_b[i:j])*2.6
+                    # 後ろから2文字目の得点変化率を強くする
+                    elif count == word_len - 2:
+                        score += len(word_b[i:j])*2.5
+                        count += 1
+                    # 文頭の得点変化率を強くする
+                    elif count == 0:
+                        score += len(word_b[i:j])*1.5
+                        count += 1
+                    # 文頭・末尾以外の処理
+                    else:
+                        score += len(word_b[i:j])
+                        count += 1
+
+       # 読み込む文字列がリストよりも短い場合
+    else:
+       # 文字列が長すぎる場合、マイナス報酬を与える
+        score += (len(word_a) - len(word_b))*1.2
+
+        word_len = len(word_a)
+        for i in range(word_len):
+            for j in range(word_len + 1):
+                if word_a[i:j] in word_b:
+
+                    # 後ろから2文字目の得点変化率を強くする
+                    if count == word_len-2:
+                        score += len(word_a[i:j])*2.5
+                        count += 1
+                    # 文末の得点変化率を強くする
+                    elif count == word_len-1:
+                        score += len(word_a[i:j])*3.6
+                    # 文頭の得点変化率を強くする
+                    elif count == 0:
+                        score += len(word_a[i:j])*1.5
+                        count += 1
+                    # 文頭・末尾以外の処理
+                    else:
+                        score += len(word_a[i:j])
+                        count += 1
+    return score
+# それぞれ母音のみにしたデータと任意の言葉を渡す。後に元の言葉が分かるようインデックスとスコアをセットで取得。
+
+
+def get_idx_score(vowel_data, target_word):
+    ranking = []
+    for i, word_b in enumerate(vowel_data):
+        score = make_score(target_word, word_b)
+        ranking.append([i, score])
+
+    return sorted(ranking, key=lambda x: -x[1])
+
+
+def get_vowel(doc):
+    return re.findall('[aiueo]', doc)
+
+
+def convert_to_romaji(text):
+
+    kakasi = pykakasi.kakasi()
+    kakasi.setMode("H", "a")        # Hiragana to ascii
+    kakasi.setMode("K", "a")        # Katakana to ascii
+    kakasi.setMode("J", "a")        # Japanese to ascii
+    kakasi.setMode("r", "Hepburn")  # use Hepburn Roman table
+    kakasi.setMode("s", True)       # add space
+    kakasi.setMode("C", False)      # no capitalize
+    conv = kakasi.getConverter()
+    result = conv.do(text)
+    return result
--- main.py
+++ main.py
@@ -8,16 +8,17 @@ from linebot.exceptions import (
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
+    QuickReplyButton, MessageAction, QuickReply, StickerSendMessage, FollowEvent
 )
 import os
-import dajaregen as dj
-import kaiseki as kk
+import fatWord as fw
+import random
 
 app = Flask(__name__)
 
 # 環境変数取得
-# YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-# YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+#YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+#YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 # 自分のチャネルトークンを書く、Herokuデプロイ時は不要
 line_bot_api = LineBotApi(
@@ -25,8 +26,8 @@ line_bot_api = LineBotApi(
 # 自分のチャネルシークレットを書く、Herokuデプロイ時は不要
 handler = WebhookHandler('168c266a7d5b15a5730f456703c7160b')
 
-# line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-# handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+#line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+#handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 
 @app.route("/")
@@ -52,15 +53,48 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    word = event.message.text
-#    dajare_text = dj.dajare_search(word)
-    kaiseki_text = kk.word_kaiseki(word)
+@handler.add(FollowEvent)
+def handle_follow(event):
     line_bot_api.reply_message(
         event.reply_token,
-        #        TextSendMessage(text=dajare_text))
-        TextSendMessage(text=kaiseki_text))
+        TextSendMessage(text='友達追加ありがとう！\n遊び方ガイドはこちら↓\n '
+                             'https://note.com/roast_official/n/ndc7d00f38d44\n「まんざい」と入力してみてね！')
+    )
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    language_list = ["やかましいわ！", "知らんがな", "欧米かっ！", "ちょっと何言ってるかわからない",
+                     "太るって！", "病院行きな", "食べすぎだよ", "やかましいわ"]
+    items = [QuickReplyButton(action=MessageAction(label=f"{language}", text=f"{language}"))
+             for language in language_list]
+
+    if event.message.text in language_list:
+        sticker_list = [['11537', 52002750], ['11537', 52002751], ['11537', 52002763],
+                        ['11538', 51626501], ['11538', 51626506], ['11538', 51626515]]
+        r = random.randint(0, 5)
+
+        # スタンプを返す
+        line_bot_api.reply_message(
+            event.reply_token,
+            StickerSendMessage(package_id=sticker_list[r][0], sticker_id=sticker_list[r][1]))
+
+    elif event.message.text == "まんざい" or "漫才":
+        # 本来はここで韻を踏んだもの(text)を受け取って送る
+        messages = TextSendMessage(
+            text="ぜんざい", quick_reply=QuickReply(items=items))
+        line_bot_api.reply_message(event.reply_token, messages=messages)
+
+    else:
+        word = event.message.text
+    #    dajare_text = dj.dajare_search(word)
+    #    kaiseki_text = kk.convert_to_romaji(word)
+    #    kaiseki_text = lb.message_generate(word)
+    #    kaiseki_text = op.message_generate(word)
+        kaiseki_text = fw.message_generate(word)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=kaiseki_text))
 
 
 if __name__ == "__main__":
