9bc69a90c7c360792b36a52b1e66bc8876748499
root <root@DESKTOP-GNI3ULF.localdomain>
2021-03-13 13:16:23 +0900

    bug fix

--- main.py
+++ main.py
@@ -49,28 +49,18 @@ def callback():
     return 'OK'
 
 
-@handler.add(FollowEvent)
-def handle_follow(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        [TextSendMessage(text='友達追加ありがとう！\n遊び方ガイドはこちら↓\n '
-                              'https://note.com/roast_official/n/ndc7d00f38d44'),
-         TextSendMessage(text='「まんざい」と入力してみてね！')]
-    )
-
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    tsukkomi_list = ["やかましいわ！", "知らんがな", "欧米かっ！", "ちょっと何言ってるかわからない",
-                     "太るって！", "病院行きな", "食べすぎだよ"]
+    language_list = ["やかましいわ！", "知らんがな", "欧米かっ！", "ちょっと何言ってるかわからない",
+                     "太るって！", "病院行きな", "食べすぎだよ", "やかましいわ"]
     items = [QuickReplyButton(action=MessageAction(label=f"{language}", text=f"{language}"))
-             for language in tsukkomi_list]
-    sticker_list = [['11537', 52002750], ['11537', 52002751], ['11537', 52002763],
-                    ['11538', 51626501], ['11538', 51626506], ['11538', 51626515]]
+             for language in language_list]
 
-    # クイックリプライメッセージを受け取ったとき
-    if event.message.text in tsukkomi_list:
+    if event.message.text in language_list:
+        sticker_list = [['11537', 52002750], ['11537', 52002751], ['11537', 52002763],
+                        ['11538', 51626501], ['11538', 51626506], ['11538', 51626515]]
         r = random.randint(0, 5)
+
         # スタンプを返す
         line_bot_api.reply_message(
             event.reply_token,
@@ -82,39 +72,49 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, messages=messages)
 
     # 「コマンド」を受け取ったとき
-    # elif event.message.text == "コマンド":
-    #     messages = TextSendMessage("ルマンド", quick_reply=QuickReply(items=items))
-    #     line_bot_api.reply_message(event.reply_token, messages=messages)
+    elif event.message.text == "コマンド":
+        messages = TextSendMessage("ルマンド", quick_reply=QuickReply(items=items))
+        line_bot_api.reply_message(event.reply_token, messages=messages)
 
     # 受け取ったメッセージが10字より大きいとき
     elif len(event.message.text) > 10:
         line_bot_api.reply_message(
             event.reply_token, TextSendMessage("単語が長いよ！" + "\uDBC0\uDC8F"))
 
-    elif re.compile(r'^[0-9]+$').match(event.message.text) is not None:
-        if re.compile(r'^[0-9]+3$').match(event.message.text) is not None or int(event.message.text) % 3 == 0:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(
-                event.message.text + "\uDBC0\uDC9D"))
+    # 3の倍数のときにぼける
+    elif event.message.text.isdigit():
+        i = int(event.message.text)
+        # print(i)
+        if i % 3 == 0:
+            sticker_list = [['11537', 52002750], ['11537', 52002751], ['11537', 52002763],
+                            ['11538', 51626501], ['11538', 51626506], ['11538', 51626515]]
+            r = random.randint(0, 5)
+            line_bot_api.reply_message(
+                event.reply_token,
+                StickerSendMessage(package_id=sticker_list[r][0], sticker_id=sticker_list[r][1]))
+        elif '3' in str(i):
+            line_bot_api.reply_message(
+                event.reply_token, TextSendMessage("3がついています"))
         else:
             line_bot_api.reply_message(
-                event.reply_token, TextSendMessage(event.message.text))
+                event.reply_token, TextSendMessage("Pardon?" + "\uDBC0\uDC9F"))
 
-    elif re.compile(r'^[0-9a-zA-Z_!"#$%&-+:/\\ \']+$').match(event.message.text) is not None:
+    elif re.compile(r'^[a-zA-Z0-9_!"#$%&-+:/\\ \']+$').match(event.message.text) is not None:
+        print(type(event.message.text))
         line_bot_api.reply_message(
             event.reply_token, TextSendMessage("Pardon?" + "\uDBC0\uDC9F"))
+#            event.reply_token, TextSendMessage(type(event.message.text)))
 
+        # elif type(event.message) == 'sticker':
+        #     line_bot_api.reply_message(
+        #         event.reply_token,
+        #         StickerSendMessage(package_id=sticker_list[4][0], sticker_id=sticker_list[4][1]))
     else:
-        # 韻を踏んだもの(reply_text)を受け取って送る
-        reply_text = fatWord.message_generate(event.message.text)
-        messages = TextSendMessage(
-            reply_text, quick_reply=QuickReply(items=items))
-        line_bot_api.reply_message(event.reply_token, messages=messages)
-    # else:
-    #     # 韻を踏んだもの(reply_text)を受け取って送る
-    #     # reply_text = main(event.message.text)
-    #     reply_text = "ちょっと何言ってるか分からない" + "\uDBC0\uDC86"
-    #     messages = TextSendMessage(reply_text, quick_reply=QuickReply(items=items))
-    #     line_bot_api.reply_message(event.reply_token, messages=messages)
+        word = event.message.text
+        kaiseki_text = fw.message_generate(word)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=kaiseki_text))
 
 
 # スタンプメッセージを受け取ったとき
